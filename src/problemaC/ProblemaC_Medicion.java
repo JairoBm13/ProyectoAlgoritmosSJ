7a8b8c3096c267bb39b41280ee75ad6865d1a027
Santiago <s.beltran10@uniandes.edu.co>
2014-11-30 20:38:08 -0500

    ??

--- src/problemaC/ProblemaC_Medicion.java
+++ src/problemaC/ProblemaC_Medicion.java
@@ -6,9 +6,9 @@ public class ProblemaC_Medicion {
 	
 	
 	public static void main(String[] args)throws Exception{
-		CasoPrueba caso=CasoPrueba.Small;
+		CasoPrueba caso=CasoPrueba.Big;
 		prepararMedicion("problemaC"+caso);
-		ProblemaC_2.main();
+		ProblemaC_1.main();
 		terminarMedicion("problemaC"+caso);
 	}
 }
